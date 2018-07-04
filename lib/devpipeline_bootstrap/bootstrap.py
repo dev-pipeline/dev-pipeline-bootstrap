92ecc00a5712aff3b48c664095f20bb063269387
Stephen Newell <sjnewell@gmail.com>
2018-07-04 14:04:40 -0600

    Initial import
    The bootstrap driver was migrated from the main dev-pipeline repository.

---
+++ lib/devpipeline_bootstrap/bootstrap.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module does a checkout and build of the packages given in the config file."""
+import devpipeline_core.command
+
+import devpipeline_build.builder
+import devpipeline_scm.scm
+
+
+def main(args=None):
+    # pylint: disable=bad-continuation,missing-docstring
+    builder = devpipeline_core.command.TargetTool([
+        devpipeline_scm.scm.scm_task,
+        devpipeline_build.builder.build_task
+    ],
+        prog="dev-pipeline bootstrap",
+        description="Checkout and build packages")
+    devpipeline_core.command.execute_tool(builder, args)
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+from setuptools import setup
+
+setup(
+    name="dev-pipeline-bootstrap",
+    version="0.2.0",
+    package_dir={
+        "": "lib"
+    },
+    packages=['devpipeline_bootstrap'],
+
+    install_requires=[
+        'dev-pipeline-core >= 0.2.0',
+        'dev-pipeline-build >= 0.2.0',
+        'dev-pipeline-scm >= 0.2.0'
+    ],
+
+    entry_points={
+        'devpipeline.drivers': [
+            'bootstrap = devpipeline_bootstrap.bootstrap:main'
+        ]
+    },
+
+    author="Stephen Newell",
+    description="build tooling for dev-pipeline",
+    license="BSD-2"
+)
