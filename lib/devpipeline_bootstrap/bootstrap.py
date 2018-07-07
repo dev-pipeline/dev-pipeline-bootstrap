c937e262ba9a978cd8849515aa5c10eb9ea425ee
Stephen Newell <sjnewell@gmail.com>
2018-07-07 12:14:23 -0600

    Use new Command API

--- lib/devpipeline_bootstrap/bootstrap.py
+++ lib/devpipeline_bootstrap/bootstrap.py
@@ -8,13 +8,13 @@ import devpipeline_scm.scm
 
 def main(args=None):
     # pylint: disable=bad-continuation,missing-docstring
-    builder = devpipeline_core.command.TargetTool([
+    builder = devpipeline_core.command.make_command([
         devpipeline_scm.scm.scm_task,
         devpipeline_build.builder.build_task
     ],
         prog="dev-pipeline bootstrap",
         description="Checkout and build packages")
-    devpipeline_core.command.execute_tool(builder, args)
+    devpipeline_core.command.execute_command(builder, args)
 
 
 if __name__ == '__main__':
